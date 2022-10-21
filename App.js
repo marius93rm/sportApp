420f1bf86c699a81270ce68a5d861206ac58649d
marius93rm <marius93rm@gmail.com>
2022-10-21 11:08:47 +0200

    Sport fondamneta

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Benvenuto</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    textTransform: 'uppercase'
+  }
 });
