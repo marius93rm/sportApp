6f378edd11ded0a5e08346146a034519f4606dbf
marius93rm <marius93rm@gmail.com>
2022-10-21 12:02:45 +0200

    flex i love you

--- App.js
+++ App.js
@@ -4,7 +4,20 @@ import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity } 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Benvenuto</Text>
+      <ImageBackground source={require('./assets/background.jpg')} style={styles.image}>
+
+        <View style={styles.sopra} >
+          <Text style={styles.header}>Benvenuto</Text>
+          <TextInput style={styles.input} placeholder="Inserisci nome" />
+        </View>
+
+        <View style={styles.sotto} >
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.label}> Cliccami </Text>
+          </TouchableOpacity>
+        </View>
+
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,10 +26,56 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  sopra: {
+    flex: 5,
+    backgroundColor: 'rgba(0,0,0,0.5)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  sotto: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+
+
+
   header: {
-    textTransform: 'uppercase'
+    textTransform: 'uppercase',
+    fontSize: 60,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  input: {
+    width: 300,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+  },
+  button: {
+    width: 300,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // position: 'absolute',
+    // bottom: 40,
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: 'bold',
   }
 });
